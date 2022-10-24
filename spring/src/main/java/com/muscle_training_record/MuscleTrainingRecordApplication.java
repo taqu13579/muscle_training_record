ff2ff2ff73c313e7138e5ba2def42839e2a5ac5d
taqu13579 <92580230+taqu13579@users.noreply.github.com>
2022-10-24 15:46:41 +0900

    Initial commit

---
+++ spring/src/main/java/com/muscle_training_record/MuscleTrainingRecordApplication.java
@@ -0,0 +1,13 @@
+package com.muscle_training_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscleTrainingRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuscleTrainingRecordApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/muscle_training_record/MuscleTrainingRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscle_training_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleTrainingRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
